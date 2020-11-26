b31d4a55149b7811524c99e6f8854981b98451c0
nipunaupeksha <nipuna.upeksha@gmail.com>
2020-11-26 18:16:23 +0530

    files changed

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,89 @@
 import React from 'react';
 import {
     StyleSheet,
+    SafeAreaView,
     View,
     Text,
-    SafeAreaView,
+    Image,
     TouchableOpacity,
-    Image
-} from 'react-native';
+    FlatList
+} from 'react-native'
 
-import {images, icons , COLORS, FONTS,SIZES} from "../constants";
+import { images, icons, COLORS, FONTS, SIZES } from '../constants';
 
-const Home = ()=>{
+const ScrollableTab = ({ tabList, selectedTab, onPress }) => {
 
+    const renderItem = ({ item }) => (
+        <TouchableOpacity
+            style={{ marginHorizontal: SIZES.padding }}
+            onPress={() => onPress(item)}
+        >
+            <Text style={{ color: COLORS.secondary, ...FONTS.body2 }}>{item.name}</Text>
 
-    const [tabList, setTabList]=React.useState([
+            {
+                selectedTab.id == item.id &&
+                <View style={{ alignItems: 'center', marginTop: SIZES.base }}>
+                    <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: COLORS.blue }}></View>
+                </View>
+            }
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={{ marginTop: SIZES.padding }}>
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={tabList}
+                renderItem={renderItem}
+                keyExtractor={item => `${item.id}`}
+            />
+        </View>
+    )
+}
+
+const ScrollableCard = ({ navigation, productList }) => {
+
+    const renderCard = ({ item }) => (
+        <TouchableOpacity
+            style={{ marginLeft: SIZES.padding }}
+            onPress={() => navigation.navigate("ItemDetail", { "itemInfo": item })}
+        >
+            <View style={{ width: SIZES.width / 2 }}>
+                <Image
+                    source={item.image}
+                    resizeMode="cover"
+                    style={{ width: '100%', height: '100%', borderRadius: SIZES.radius * 2 }}
+                />
+
+                <View style={{ position: 'absolute', top: 15, left: '10%', right: '10%' }}>
+                    <Text style={{ color: COLORS.lightGray2, ...FONTS.h3 }}>Furniture</Text>
+                    <Text style={{ marginTop: SIZES.base, color: COLORS.white, ...FONTS.h2 }}>{item.productName}</Text>
+                </View>
+
+                <View style={{ position: 'absolute', bottom: 20, left: 30, borderRadius: 15, paddingVertical: 10, paddingHorizontal: 15, backgroundColor: COLORS.transparentLightGray }}>
+                    <Text style={{ ...FONTS.h2 }}>$ {item.price.toFixed(2)}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+
+    return (
+        <View style={{ marginTop: SIZES.padding }}>
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={productList}
+                renderItem={renderCard}
+                keyExtractor={item => `${item.productId}`}
+            />
+        </View>
+    )
+}
+
+const Home = ({ navigation }) => {
+
+    const [tabList, setTabList] = React.useState([
         {
             id: 0,
             name: "Chair",
@@ -117,7 +187,7 @@ const Home = ()=>{
 
             ]
         }
-    ]);
+    ])
 
     const [selectedTab, setSelectedTab] = React.useState({
         id: 0,
@@ -146,7 +216,8 @@ const Home = ()=>{
         ]
     })
 
-    //Render
+    // Render
+
     function renderHeader() {
         return (
             <View style={{ paddingHorizontal: SIZES.padding }}>
@@ -185,7 +256,7 @@ const Home = ()=>{
         )
     }
 
-    function renderTitle(title){
+    function renderTitle(title) {
         return (
             <View style={{ marginTop: SIZES.padding, marginHorizontal: SIZES.padding }}>
                 <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>Collection of</Text>
@@ -194,31 +265,111 @@ const Home = ()=>{
         )
     }
 
-    return(
+    function renderPromotionCard() {
+        return (
+            <View
+                style={[styles.shadow, {
+                    flexDirection: 'row',
+                    marginHorizontal: SIZES.padding,
+                    padding: SIZES.radius,
+                    height: 110,
+                    borderRadius: 20,
+                    backgroundColor: COLORS.white
+                }]}
+            >
+                <View
+                    style={{
+                        width: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: COLORS.lightGray2,
+                        borderRadius: 20
+                    }}
+                >
+                    <Image
+                        source={images.sofa}
+                        resizeMode="contain"
+                        style={{
+                            width: '60%',
+                            height: '60%'
+                        }}
+                    />
+                </View>
+
+                {/* Wordings section */}
+                <View style={{ flex: 1, marginLeft: SIZES.radius, justifyContent: 'center' }}>
+                    <Text style={{ ...FONTS.h2 }}>Special Offer</Text>
+                    <Text style={{ ...FONTS.body3 }}>Adding to your cart</Text>
+                </View>
+
+                {/* Button */}
+                <View style={{ marginRight: SIZES.radius, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity
+                        style={{
+                            backgroundColor: COLORS.primary,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '70%',
+                            width: 40,
+                            borderRadius: 10
+                        }}
+                        onPress={() => { console.log("Promo on clicked") }}
+                    >
+                        <Image
+                            source={icons.chevron}
+                            resizeMode="contain"
+                            style={{
+                                height: '50%',
+                                width: '50%'
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+
+    return (
         <SafeAreaView style={styles.container}>
             {renderHeader()}
+
             {renderTitle(selectedTab.title)}
+
             <ScrollableTab
                 tabList={tabList}
+                selectedTab={selectedTab}
+                onPress={(item) => setSelectedTab(item)}
             />
+
+            <View style={{ flex: 1 }}>
+                <ScrollableCard
+                    navigation={navigation}
+                    productList={selectedTab.productList}
+                />
+            </View>
+
+            {/* Footer - Promotion Card */}
+            <View style={{ height: "19%", justifyContent: 'flex-end' }}>
+                {renderPromotionCard()}
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:COLORS.white
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white
     },
-    shadow:{
-        shadowColor:"#000",
-        shadowOffset:{
-            width:0,
-            height:3
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
         },
         shadowOpacity: 0.32,
         shadowRadius: 5.46,
-        elevation: 9
+        elevation: 9,
     }
 })
 
