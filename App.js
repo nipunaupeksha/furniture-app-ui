50e5c8a69f99be2e3f2077a16f69d88c110cebb6
nipunaupeksha <nipuna.upeksha@gmail.com>
2020-11-26 15:22:05 +0530

    files added

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// screens
+import { Home, ItemDetail } from "./screens/";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="ItemDetail" component={ItemDetail} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+
+export default () => {
+    return <App />;
+};
---
+++ screens/Home.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    SafeAreaView,
+    TouchableOpacity,
+    Image
+} from 'react-native';
+
+import {images, icons , COLORS, FONTS,SIZES} from "../constants";
+
+const Home = ()=>{
+
+
+    const [tabList, setTabList]=React.useState([
+        {
+            id: 0,
+            name: "Chair",
+            title: "chairs",
+            productList: [
+                {
+                    productId: 1,
+                    productName: 'Chair Green Colour',
+                    price: 10.00,
+                    image: images.greenChair,
+                },
+                {
+                    productId: 2,
+                    productName: 'Chair Peach Colour',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 3,
+                    productName: 'Chair White Colour',
+                    price: 10.00,
+                    image: images.whiteChair,
+                },
+            ]
+        },
+        {
+            id: 1,
+            name: "Cupboard",
+            title: 'cupboards',
+            productList: [
+                {
+                    productId: 4,
+                    productName: 'Product 4',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 5,
+                    productName: 'Product 5',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 6,
+                    productName: 'Product 6',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        },
+        {
+            id: 2,
+            name: "Table",
+            title: 'tables',
+            productList: [
+                {
+                    productId: 7,
+                    productName: 'Product 7',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 8,
+                    productName: 'Product 8',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 9,
+                    productName: 'Product 9',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        },
+        {
+            id: 3,
+            name: "Accessories",
+            title: 'accessories',
+            productList: [
+                {
+                    productId: 10,
+                    productName: 'Product 10',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 11,
+                    productName: 'Product 11',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+                {
+                    productId: 12,
+                    productName: 'Product 12',
+                    price: 10.00,
+                    image: images.redChair,
+                },
+
+            ]
+        }
+    ]);
+
+    const [selectedTab, setSelectedTab] = React.useState({
+        id: 0,
+        name: "Chair",
+        title: 'chairs',
+        productList: [
+            {
+                productId: 1,
+                productName: 'Chair Green Colour',
+                price: 10.00,
+                image: images.greenChair,
+            },
+            {
+                productId: 2,
+                productName: 'Chair Peach Colour',
+                price: 10.00,
+                image: images.redChair,
+            },
+            {
+                productId: 3,
+                productName: 'Chair White Colour',
+                price: 10.00,
+                image: images.whiteChair,
+            },
+
+        ]
+    })
+
+    //Render
+    function renderHeader() {
+        return (
+            <View style={{ paddingHorizontal: SIZES.padding }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                        <TouchableOpacity
+                            onPress={() => console.log("Menu on clicked")}
+                        >
+                            <Image
+                                source={icons.menu}
+                                resizeMode="contain"
+                                style={{
+                                    width: 25,
+                                    height: 25
+                                }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={{ flex: 1, alignItems: 'flex-end' }}>
+                        <TouchableOpacity
+                            onPress={() => { console.log("Cart on clicked") }}
+                        >
+                            <Image
+                                source={icons.cart}
+                                resizeMode="contain"
+                                style={{
+                                    width: 25,
+                                    height: 25
+                                }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+
+    function renderTitle(title){
+        return (
+            <View style={{ marginTop: SIZES.padding, marginHorizontal: SIZES.padding }}>
+                <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>Collection of</Text>
+                <Text style={{ color: COLORS.black, ...FONTS.largeTitle }}>{title}</Text>
+            </View>
+        )
+    }
+
+    return(
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+            {renderTitle(selectedTab.title)}
+            <ScrollableTab
+                tabList={tabList}
+            />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:COLORS.white
+    },
+    shadow:{
+        shadowColor:"#000",
+        shadowOffset:{
+            width:0,
+            height:3
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9
+    }
+})
+
+export default Home;
